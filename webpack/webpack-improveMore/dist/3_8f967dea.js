5972e2c601f825fa01a2d54dd2458b72019f867e
machengrong@css.com.cn <machengrong@css.com.cn>
2020-01-19 14:20:02 +0800

    update:webpack配置

---
+++ webpack/webpack-improveMore/dist/3_8f967dea.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{12:function(n,e,t){"use strict";t.r(e);var i=t(0),o=t.n(i);e.default=function(){return o.a.createElement("div",null,"动态import")}}}]);
--- webpack/webpack-improveMore/src/index/index.js
+++ webpack/webpack-improveMore/src/index/index.js
@@ -10,11 +10,31 @@ import './index.css'
 import { a } from '../tree-shaking'
 
 class Index extends React.Component{
+    constructor(){
+        super(...arguments)
+        this.state= {
+            Text:null
+        }
+    }
+    loadText() {
+        import('./text.js').then((text)=>{
+            this.setState({
+                Text: text.default
+            })
+        })
+    }
     render(){
+        const { Text } = this.state
+        console.log(Text)
         const val = a()
         return (
             <div className='index'>
                 index page,{val}
+                <hr/>
+                {
+                    Text ? <Text/> : null
+                }
+                <span onClick={this.loadText.bind(this)}>loadText</span>
             </div>
         )
     }
